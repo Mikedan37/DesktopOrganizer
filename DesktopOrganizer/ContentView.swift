8b2716440c61eee4941c9558e45949115663f1d9
Michael Danylchuk <mn_danylchuk@yahoo.com>
2025-08-01 23:09:17 -0700

    Initial Commit

---
+++ DesktopOrganizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesktopOrganizer
+//
+//  Created by Michael Danylchuk on 8/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesktopOrganizer/DesktopOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopOrganizerApp.swift
+//  DesktopOrganizer
+//
+//  Created by Michael Danylchuk on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DesktopOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
